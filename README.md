0eafcd47bcd16fdd6dd62e2797a52fc5a247e9fc
Adrien Moreau <adrien@nash.io>
2021-12-10 14:54:42 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Canard
+
+**TODO: Add description**
+
---
+++ apps/my_app_2/lib/my_app2.ex
@@ -0,0 +1,18 @@
+defmodule MyApp2 do
+  @moduledoc """
+  Documentation for `MyApp2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MyApp2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
